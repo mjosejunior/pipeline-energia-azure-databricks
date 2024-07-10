88b56283495ca28d42292afc369b1dc2d9cdd3fc
mjosejunior <mjosejunior>
2024-07-10 02:32:04 +0000

    Feat: Lista informações das camadas (Bronze, Silver e Gold)

---
+++ exibe_info.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %fs ls /mnt/Aneel/Bronze/
+
+# COMMAND ----------
+
+# MAGIC %fs ls /mnt/Aneel/Silver/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/Aneel/Silver/dominio_indicadores_silver/
+
+# COMMAND ----------
+
+# MAGIC %fs ls /mnt/Aneel/Gold/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/Aneel/Gold/dim_agente/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/Aneel/Gold/fact_inadimplencia/
